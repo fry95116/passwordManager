77332107705a51e1970c7a9c153c8f0bcfc1b8c6
TastyCarb <fry95116@gmail.com>
2018-06-25 19:41:53 +0800

    添加摄像头切换

--- src/js/views/QRcodeReader.js
+++ src/js/views/QRcodeReader.js
@@ -19,8 +19,8 @@ var QRcodeReader = new Vue({
                 <div class="tips">将二维码放入取景框中</div>
             </div>
             <button class="btn btn-cancel" @click="cancel">&lt; 返回</button>
-            <button class="btn btn-switch">切换设备</button>
-            <button class="btn btn-album">相册</button>
+            <button v-if="btn_switchCamera" class="btn btn-switch" @click="switch">切换设备</button>
+            <button v-if="btn_readFromAlbum" class="btn btn-album">相册</button>
 
             <canvas style="display:none"></canvas>
             <input v-if="!support_UserMedia" class="fileReader-camera" type="file" accept="image/*" capture="camera">
@@ -30,8 +30,8 @@ var QRcodeReader = new Vue({
     `,
     data:{
         hidden: true,
-        switchCamera: true,
-        readFromAlbum: false,
+        btn_switchCamera: true,
+        btn_readFromAlbum: true,
         $_isFront: true
     },
     computed:{
@@ -45,30 +45,11 @@ var QRcodeReader = new Vue({
 
     methods:{
         async readQRcode(){
-
-            const CONSTRAINTS_DEFAULT      = { video: true, audio: false }
-            const CONSTRAINTS_CAMERA_FRONT = { video: { facingMode: 'user' }, audio: false }
-            const CONSTRAINTS_CAMERA_BACK  = { video: { facingMode: 'environment' }, audio: false }
-        
             if(this.support_UserMedia){
                 var mediaDevices = util.getMediaDevices()
                 // if (mediaDevices === null) throw new Error('mediaDevices API not supported')
-
-                var constraints = CONSTRAINTS_DEFAULT
-                if (this.switchCamera) {
-                    constraints = this.$_isFront ? CONSTRAINTS_CAMERA_FRONT : CONSTRAINTS_CAMERA_BACK
-                }
-
-                try{
-                    var stream = await mediaDevices.getUserMedia(constraints)
-                }
-                catch(err) {
-                    throw err
-                }
-
-                this.$el.querySelector('video').srcObject = stream
+                await this.setCamera()
                 this.hidden = false
-
             }
             else{
                 this.$el.querySelector('.fileReader-camera').click()
@@ -76,7 +57,31 @@ var QRcodeReader = new Vue({
         },
 
         cancel(){
+            this.$el.querySelector('video').srcObject = null
             this.hidden = true
+        },
+
+        async switchCamera(){
+            this.$_isFront = !this.$_isFront
+            //TODO:add try catch
+            await this.setCamera()
+        },
+
+        async setCamera(){
+            //TODO: facingMode的支持性问题
+            // const CONSTRAINTS_DEFAULT = { video: true, audio: false }
+            const CONSTRAINTS_CAMERA_FRONT = { video: { facingMode: 'user' }, audio: false }
+            const CONSTRAINTS_CAMERA_BACK  = { video: { facingMode: 'environment' }, audio: false }
+
+            // var constraints = CONSTRAINTS_DEFAULT
+            var constraints = this.$_isFront ? CONSTRAINTS_CAMERA_FRONT : CONSTRAINTS_CAMERA_BACK
+            try{
+                var stream = await mediaDevices.getUserMedia(constraints)
+            }
+            catch(err) {
+                throw err
+            }
+            this.$el.querySelector('video').srcObject = stream
         }
     }
 })
